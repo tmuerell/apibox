732237b70334f5efa0190b3a76de843111c17468
Thorsten Mürell <thorsten@muerell.de>
2021-02-10 08:27:21 +0100

    Some formatting and highlighting

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,9 +7,13 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import hljs from 'highlight.js';
+import hljs from 'highlight.js'
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 hljs.highlightAll()
+
+export {
+    hljs
+}
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,10 @@
 const { environment } = require('@rails/webpacker')
 
 module.exports = environment
+
+environment.config.merge({
+    output: {
+      library: ['Packs', '[name]'], // exports to "Packs.application" from application pack
+      libraryTarget: 'var',
+    }
+  })

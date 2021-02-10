6b3fb9a3efd0fea39500e95858e5eaa5d53e2adc
Thorsten Mürell <thorsten@muerell.de>
2021-02-10 03:28:22 +0100

    Fixed typo

--- db/migrate/20210209221006_create_folders.rb
+++ db/migrate/20210209221006_create_folders.rb
@@ -2,7 +2,7 @@ class CreateFolders < ActiveRecord::Migration[6.1]
   def change
     create_table :folders do |t|
       t.string :name
-      t.references :parent, null: true, { to_table: :folders }
+      t.references :parent, null: true, foreign_key: { to_table: :folders }
 
       t.timestamps
     end

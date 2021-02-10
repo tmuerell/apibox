5d9c43e5955493fbef3d356db2d7a9fbdf3bf82e
Thorsten Mürell <thorsten@muerell.de>
2021-02-10 03:23:58 +0100

    Some fixes

--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,7 @@ Rails.application.routes.draw do
     end
   end
   resources :folders
+
+  root to: "folders#index"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- db/migrate/20210209221006_create_folders.rb
+++ db/migrate/20210209221006_create_folders.rb
@@ -2,7 +2,7 @@ class CreateFolders < ActiveRecord::Migration[6.1]
   def change
     create_table :folders do |t|
       t.string :name
-      t.references :parent, null: false, foreign_key: true
+      t.references :parent, null: true, { to_table: :folders }
 
       t.timestamps
     end

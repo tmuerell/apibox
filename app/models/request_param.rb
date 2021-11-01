270f827e0767bb1b63770ef875c406d1793ede70
Thorsten Mürell <thorsten@muerell.de>
2021-11-01 06:59:09 +0100

    Some visual improvements

--- app/models/request_param.rb
+++ app/models/request_param.rb
@@ -1,4 +1,14 @@
 class RequestParam < ApplicationRecord
   belongs_to :request, optional: true
   belongs_to :request_example, optional: true
+
+  validate :belongs_to_request_or_example
+
+  protected
+
+  def belongs_to_request_or_example
+    unless self.request.present? or self.request_example.present?
+      self.errors.add :request, 'Associated object not found'
+    end
+  end
 end
